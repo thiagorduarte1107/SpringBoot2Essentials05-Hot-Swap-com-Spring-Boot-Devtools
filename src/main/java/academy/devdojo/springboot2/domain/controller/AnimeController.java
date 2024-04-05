a626489eca78f8bbe60870c2d2b8f498243f6f70
thiagorduarte1107 <73600221+thiagorduarte1107@users.noreply.github.com>
2024-04-05 16:19:10 -0300

    Update AnimeController.java

--- src/main/java/academy/devdojo/springboot2/domain/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/domain/controller/AnimeController.java
@@ -1,22 +1,40 @@
 package academy.devdojo.springboot2.domain.controller;
 
 // Importações necessárias
+
 import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.util.DateUtil;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 // Indica que esta classe é um controlador REST, permitindo que seus métodos manipulem requisições HTTP
 @RestController
 // Define o caminho base para todas as rotas neste controlador
 @RequestMapping("anime")
+@Log4j2 // Cria automaticamente um logger estático para a classe, usando Log4j2, para facilitar o logging.
+//@Autowired // Injeta automaticamente a dependência marcada neste campo.
+// @AllArgsConstructor // Construtor gerado automaticamente com todos os argumentos para as final fields e @NonNull fields.
+// @RequiredArgsConstructor // Construtor gerado automaticamente com todos os argumentos para final fields e @NonNull fields, mas não inicializa fields com valor default.
 public class AnimeController {
-    // Mapeia para GET requests para "/anime/list". Retorna uma lista de Animes.
     @GetMapping(path = "list")
     public List<Anime> list() {
-        // Retorna uma lista fixa de Animes como resposta
+        log.info(DateUtil.formatLocalDateTimeToDatebaseStyle(LocalDateTime.now()));
         return List.of(new Anime("DBZ"), new Anime("CDZ"));
     }
+
+    @GetMapping(path = "list2")
+    public List<Anime> list2() {
+        log.info(DateUtil.formatLocalDateTimeToDatebaseStyle(LocalDateTime.now()));
+        return List.of(new Anime("Xmen"), new Anime("Naruto"));
+    }
+
+
 }
